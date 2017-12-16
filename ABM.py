9fc9e40c9325775256df54d3456359e79380e7f9
HaitongWang <34452134+HaitongWang@users.noreply.github.com>
2017-12-16 18:50:29 +0000

    the first practical
    simple agent, two pair of coordinates and distance.

--- ABM.py
+++ ABM.py
@@ -2,6 +2,44 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+by Haitong Wang
 """
+import random
 
+# Set up variables.
+y0 = 50
+x0 = 50
+
+# Random walk one step.
+if random.random() < 0.5:
+    y0 += 1
+else:
+    y0 -= 1
+     
+if random.random() < 0.5:
+    x0 += 1
+else:
+    x0 -= 1 
+        
+print (y0,x0) 
+
+
+# Set up another variables.
+y1 = 50
+x1 = 50
+
+# Random walk one step.
+if random.random() < 0.5:
+    y1 += 1
+else:
+    y1 -= 1
+     
+if random.random() < 0.5:
+    x1 += 1
+else:
+    x1 -= 1 
+ 
+print (y1,x1)
+
+distance = ((y1-y0)**2 + (x1-x0)**2)**0.5 
+print (distance)
