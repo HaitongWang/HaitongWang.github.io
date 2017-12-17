33aaf01dfac1621d75a72edbfaab30273950d67c
HaitongWang <34452134+HaitongWang@users.noreply.github.com>
2017-12-17 20:02:21 +0000

    add the environment

--- ABM.py
+++ ABM.py
@@ -8,6 +8,20 @@ import matplotlib.pyplot
 import operator
 import random
 import agentframework
+import csv
+
+f = open('in.txt', newline='') 
+reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+
+environment = []  #Lines here happen before any data is processed
+
+for row in reader:
+    rowlist = []  #Lines here happen before each row is processed
+    for values in row:
+        rowlist.append(values)  #do something with values
+environment.append(rowlist)  #Lines here happen after row is processed
+
+      
 
 def distance_between(agent0, agent1): 
     return (((agent0.x - agent1.x)**2) + ((agent0.y - agent1.y)**2))**0.5
@@ -18,24 +32,29 @@ agents = []
 
 # Set up agents.
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment))
 
 # Random walk agents.(y0,x0)
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
         
         agents[i].move()
-
+        agents[i].eat()
       
 print (agents) 
 
 
-
+for agent0 in agents:
+     for agent1 in agents:
+         distance = distance_between(agent0, agent1)
 distance = distance_between(agents[0], agents[1])
 print(distance)
 
+
+
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
--- agentframework.py
+++ agentframework.py
@@ -6,14 +6,15 @@ Created on Sun Dec 17 14:14:32 2017
 """
 import random
 
-agents = []
-agents.append([random.randint(0,99),random.randint(0,99)])
 
 class Agent:
-    def __init__ (self):
-            self.x = random.randint(0,99)
-            self.y = random.randint(0,99)
-    
+        
+    def __init__ (self,environment, store):
+        self.x = random.randint(0,99)
+        self.y = random.randint(0,99)   
+        self.environment = environment
+        self.store = 0
+ 
     def move (self):
         if random.random() < 0.5:
             self.x = (int(self.x) + 1) % 100
@@ -24,6 +25,11 @@ class Agent:
             self.y = (int(self.y) + 1) % 100
         else:
             self.y = (int(self.y) + 1) % 100 
+            
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10
         
 
 def distance_between(agent0:Agent, agent1:Agent): 
